964fae3bfaf1458427a26fb3e204f398ee9915fc
Zakaria Outahar <outaharz@gmail.com>
2021-09-14 12:17:37 +0200

    Optimisation du mode plein écran

--- app.js
+++ app.js
@@ -38,23 +38,25 @@ volumeSlider.addEventListener('change', () => {
     video.volume = volumeSlider.value / 100;
     if(video.volume === 0){
         muteBtn.innerText = "Unmute";
+        video.muted = true;
     } else{
         muteBtn.innerText = "Mute";
+        video.muted = false;
     }
 })
 
 // Mute
 
-let volume;
+let volume = 40;
 
 muteBtn.addEventListener('click', () => {
     if(video.volume === 0){
-        // video.muted = false;
+        video.muted = false;
         volumeSlider.value = volume;
         video.volume = volumeSlider.value / 100;
         muteBtn.innerText = "Mute";
     } else{
-        // video.muted = true;
+        video.muted = true;
         volume = volumeSlider.value;
         volumeSlider.value = 0;
         video.volume = volumeSlider.value / 100;
@@ -77,6 +79,29 @@ barreOrange.addEventListener('click', (e) => {
 //     rect = barreOrange.getBoundingClientRect();
 //     largeur = rect.width;
 // })
+
+video.addEventListener('fullscreenchange', () => {
+    if(video.paused){
+        imgPlayPause.src = "ressources/play.svg";
+    }
+    else{
+        imgPlayPause.src = "ressources/pause.svg";
+    }
+
+    if(video.muted){
+        muteBtn.innerText = "Unmute";
+        video.volume = 0;
+        volumeSlider.value = 0;
+    } else{
+        if(video.volume === 0){
+            muteBtn.innerText = "Unmute";
+            volumeSlider.value = 0;
+        } else{
+            muteBtn.innerText = "Mute";
+            volumeSlider.value = video.volume * 100;
+        }
+    }
+})
 
 video.addEventListener('dblclick', () => {
     video.requestFullscreen();
